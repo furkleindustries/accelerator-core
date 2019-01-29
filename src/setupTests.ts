de68110ab5afaea6c78678dd0ec82fe98206b703
furkle <furkle@furkleindustries.com>
2019-01-29 16:00:03 -0500

    Refactoring configurations.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,33 +1,4 @@
 import * as Enzyme from 'enzyme';
 const Adapter = require('enzyme-adapter-react-16');
 
-import {
-  pathExistsSync,
-} from 'fs-extra';
-import {
-  join,
-} from 'path';
-
-import {
-  execSync,
-} from 'child_process';
-
-const appDir = join(__dirname, '..');
-
-if (!pathExistsSync(join(appDir, 'passages', 'passages-manifest.ts'))) {
-  require('../scripts/generatePassagesManifest');
-}
-
-if (!pathExistsSync(join(appDir, 'headers', 'headers-manifest.ts'))) {
-  require('../scripts/generateHeadersManifest');
-}
-
-if (!pathExistsSync(join(appDir, 'footers', 'footers-manifest.ts'))) {
-  require('../scripts/generateFootersManifest');
-}
-
-if (!pathExistsSync(join(appDir, 'plugins', 'plugins-manifest.ts'))) {
-  require('../scripts/generatePluginsManifest');
-}
-
 Enzyme.configure({ adapter: new Adapter() });
