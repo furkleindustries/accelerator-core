dd30de0d26cde16565bd6b37ea8fbd2d9b7a6c22
furkle <furkle@furkleindustries.com>
2019-01-29 16:33:16 -0500

    Refactored tests. They now run but many fail.

---
+++ config/jest/globalSetup.js
@@ -0,0 +1,3 @@
+module.exports = function globalSetup() {
+  require('../../scripts/generateManifests');
+};
--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -1,55 +1,48 @@
+const includedDirectories = '(footers|headers|passages|plugins|src)';
+
 module.exports = {
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts'
+  testMatch: [
+    '<rootDir>/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
   ],
 
-  resolver: 'jest-pnp-resolver',
-  setupTestFrameworkScriptFile: '<rootDir>/src/setupTests.ts',
-  globalSetup: '<rootDir>/scripts/generateManifests.js',
+  collectCoverageFrom: [
+    `**/${includedDirectories}/*.{js,jsx,ts,tsx}`,
+    '!**/*.d.ts',
+  ],
 
+  rootDir: '../../',
+
+  setupTestFrameworkScriptFile: '<rootDir>/config/jest/setupTests.ts',
+  globalSetup: '<rootDir>/config/jest/globalSetup.js',
   setupFiles: [
     'react-app-polyfill/jsdom'
   ],
 
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
-    '<rootDir>/footers/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
-    '<rootDir>/headers/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
-    '<rootDir>/passages/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
-    '<rootDir>/plugins/**/?(*.)(spec|test).{js,jsx,ts,tsx}'
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
   ],
 
   testEnvironment: 'jsdom',
   testURL: 'http://localhost',
 
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js'
-  },
-
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
-    '^.+\\.module\\.(css|sass|scss)$'
-  ],
-
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
-    '^.+\\.(sass|scss)$': 'identity-obj-proxy'
+    '^.+\\.(sass|scss)$': 'identity-obj-proxy',
   },
 
   moduleFileExtensions: [
-    'web.js',
     'js',
-    'web.ts',
     'ts',
-    'web.tsx',
     'tsx',
-    'json',
-    'web.jsx',
     'jsx',
-    'node'
-  ]
+  ],
+
+  resolver: 'jest-pnp-resolver',
 }
