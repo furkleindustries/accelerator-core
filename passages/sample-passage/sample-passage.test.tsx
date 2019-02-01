d1f99d393ce37bca7d3ac7c3f5640b86a90b2c27
furkle <furkle@furkleindustries.com>
2019-02-01 05:37:40 -0500

    Adjusting gitignore rules.

---
+++ passages/sample-passage/sample-passage.test.tsx
@@ -0,0 +1,72 @@
+import {
+  shallow,
+} from 'enzyme';
+
+import passage from './sample-passage';
+
+import {
+  BuiltInTags,
+  getTag,
+  Tag,
+} from '../../src/passages/tagsBundle';
+
+import * as React from 'react';
+
+const {
+  name,
+  tags,
+  title,
+  contents: Component,
+} = passage;
+
+const testFactory = (options?: { [key: string]: any}) => (
+  <Component
+    bookmark={jest.fn()}
+    dispatch={jest.fn()}
+    lastLinkTags={[]}
+    navigateTo={jest.fn()}
+    passageObject={{ name: 'test' } as any}
+    restart={jest.fn()}
+    rewind={jest.fn()}
+    setStoryState={jest.fn()}
+    storyState={{}}
+    {...options}
+  />
+);
+
+describe('Tests for the sample-passage passage.', () => {
+  it('Has a non-empty name string.', () => {
+    expect(name && typeof name === 'string').toBe(true);
+  });
+
+  it('If it has a title, it is a non-empty string.', () => {
+    expect(!title || typeof title === 'string').toBe(true);
+  });
+
+  it('If it has tags, they are either non-empty strings or key-value objects.', () => {
+    expect(!tags || tags.filter((aa: Tag) => {
+      if (aa) {
+        if (typeof aa === 'string') {
+          return true;
+        } else if (typeof aa === 'object') {
+          if (aa.key &&
+              typeof aa.key === 'string' &&
+              aa.value &&
+              typeof aa.value === 'string')
+          {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }).length === tags.length).toBe(true);
+  });
+
+  it('Renders shallowly without crashing.', () => {
+    /* Don't test if it's a noRender passage. */
+    if (getTag(tags, BuiltInTags.NoRender)) {
+      shallow(testFactory());
+    }
+  });
+});
